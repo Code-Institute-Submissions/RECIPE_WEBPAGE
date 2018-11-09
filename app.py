8ed3b43510483be29d0cf066a7e088aad2ca7511
Russell <russell.treagus@hotmail.co.uk>
2018-11-09 21:57:37 +0000

    DB url edit

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ connection = pymysql.connect(host=os.environ.get("DB_HOST"),
                              user=os.environ.get("DB_USER"),
                              password=os.environ.get("DB_PASSWORD"),
                              db=os.environ.get("DB_NAME"))
-             
+                        
 cursor = connection.cursor(pymysql.cursors.DictCursor)
 
 
